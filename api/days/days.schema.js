5b7eb3a8ebaf7b6381d25e5cf400382d912e1d68
mikiteek <mikiteek@gmail.com>
2020-11-28 10:24:42 +0200

    Create day schema like Natalia showed

--- api/days/days.schema.js
+++ api/days/days.schema.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 const {Schema, ObjectId} = mongoose;
 
-const daySchema = new Schema({
-  productId: ObjectId,
-  weight: Number,
-  date: Date,
-  userId: ObjectId,
+const daySchema = new Schema( {
+  productId: { type: ObjectId, require: true},
+  weight: { type: Number, require: true},
+  date: { type: Date, require: true},
+  userId: { type: ObjectId, require: true }
 });
 
 module.exports = mongoose.model("Day", daySchema);
