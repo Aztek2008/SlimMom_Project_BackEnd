84cafa07f186d4e15bd6cbdc2820cba19c07ae8c
mikiteek <mikiteek@gmail.com>
2020-11-28 10:20:06 +0200

    Refactor tests for all endpoint, fixed bug jest

---
+++ api/days/days.test.js
@@ -0,0 +1,42 @@
+const request = require("supertest");
+const StartServer = require("../server");
+const mongoose = require("mongoose");
+
+const testServer = new StartServer();
+testServer.initSevices();
+
+const app = testServer.getServer();
+
+
+describe("DELETE /days", () => {
+  beforeAll(done => {
+    done()
+  });
+  afterAll(done => {
+    mongoose.connection.close()
+    done()
+  });
+  it ("should return 201",async () => {
+    await request(app)
+      .post("/days")
+      .send({
+        productId:"5d51694802b2373622ff5534",
+        weight: 550,
+        date: "2020-11-27"
+      })
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(201)
+  });
+
+  it ('should return 200', async () => {
+    await request(app)
+      .delete("/days")
+      .send({
+        "productId":"5d51694802b2373622ff5534",
+        "date": "2020-11-27"
+      })
+      .set("Accept", "application/json")
+      .expect(200)
+  });
+});
