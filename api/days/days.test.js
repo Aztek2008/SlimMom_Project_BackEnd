b1cadf7183be406191da4b1afbe8dd9039f0b9b5
mikiteek <mikiteek@gmail.com>
2020-11-29 18:18:05 +0200

    Add tests for GET /days

--- api/days/days.test.js
+++ api/days/days.test.js
@@ -16,7 +16,7 @@ describe("Correct work for endpoint /days", () => {
     mongoose.connection.close()
     done()
   });
-  let token, dayId;
+  let token, dayId, date;
 
   describe("POST /days", () => {
     beforeEach(async () => {
@@ -42,7 +42,7 @@ describe("Correct work for endpoint /days", () => {
         .set("Accept", "application/json")
         .set("Authorization", "Bearer " + token)
         .expect(201)
-      dayId = responseCreate.body;
+      dayId = responseCreate.body._id;
     });
 
   });
@@ -70,5 +70,54 @@ describe("Correct work for endpoint /days", () => {
         .set("Authorization", "Bearer " + token)
         .expect(200)
     });
-  })
+  });
+
+  describe("GET /days", () => {
+    beforeEach (async () => {
+      const responseLogin = await request(app)
+        .post("/users/login")
+        .send({
+          login:"mikiteek@gmail.com",
+          password: "111111"
+        })
+        .set("Accept", "application/json")
+        .expect(200)
+      token = responseLogin.body.token;
+
+      const responseCreate = await request(app)
+        .post("/days")
+        .send({
+          productId:"5d51694802b2373622ff5534",
+          weight: 550,
+          date: "2020-11-27"
+        })
+        .set("Accept", "application/json")
+        .set("Authorization", "Bearer " + token)
+        .expect(201)
+      date = responseCreate.body.date;
+      dayId = responseCreate.body._id;
+    });
+
+    afterEach(async () => {
+      await request(app)
+        .delete("/days")
+        .send({
+          dayId
+        })
+        .set("Accept", "application/json")
+        .set("Authorization", "Bearer " + token)
+        .expect(200)
+    })
+
+    it ('should return 200', async () => {
+      await request(app)
+        .get("/days")
+        .send({
+          date
+        })
+        .set("Accept", "application/json")
+        .set("Authorization", "Bearer " + token)
+        .expect(200)
+    });
+  });
 });
