c438e76daea1be40f1b6b742556163e4a9f96933
Aabondd <aabondd@gmail.com>
2020-12-01 17:49:52 +0200

    Pulic daily calories calculator & prohibited food categories

---
+++ api/products/products.helpers.js
@@ -0,0 +1,30 @@
+const { json } = require("express");
+const Product = require("./products.schema");
+
+const getNotAllowedCategoryProducts = async (groupBlood) => {
+  let queryParams;
+  switch (groupBlood) {
+    case 1:
+      queryParams = { "groupBloodNotAllowed.1": true };
+      break;
+    case 2:
+      queryParams = { "groupBloodNotAllowed.2": true };
+      break;
+    case 3:
+      queryParams = { "groupBloodNotAllowed.3": true };
+      break;
+    case 4:
+      queryParams = { "groupBloodNotAllowed.4": true };
+      break;
+    default:
+      return false;
+  }
+  return await Product.distinct("categories", queryParams);
+};
+
+// const categories = await getNotAllowedCategoryProducts(4);
+// console.log(categories);
+
+module.exports = {
+  getNotAllowedCategoryProducts,
+};
